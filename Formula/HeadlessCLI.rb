3e5a20a386c4a23437429ed8001c94863349a7fa
Isaac Sneddon-Jenkins <info.isaacsneddon@gmail.com>
2024-09-17 13:25:42 +0100

    Update HeadlessCLI.rb

--- Formula/HeadlessCLI.rb
+++ Formula/HeadlessCLI.rb
@@ -1,4 +1,4 @@
-class HeadlessCLI < Formula
+class HeadlessCli < Formula
   desc "CLI for accessing templates/boilerplates for Headless sites via the CLI"
   homepage "https://github.com/sneddonisaac/headless-cli"
   url "https://github.com/sneddonisaac/headless-cli/releases/download/Test/headless_cli-0.1.0-x86_64-apple-darwin.tar.gz"
